7812278aae2241557e4b67fa26174bc8e2b53106
tefuirluo <tefuirluo@163.com>
2023-02-21 10:30:47 +0800

    下拉刷新

--- pages/main-vided/main-vided.js
+++ pages/main-vided/main-vided.js
@@ -25,10 +25,23 @@ Page({
 		this.data.offset = this.data.videoList.length
 		this.data.hasMore = res.hasMore
 	},
+	// 下拉加载更多
 	onReachBottom(){
 		// 1. 判断是否有更多数据
 		if(!this.data.hasMore) return
 		// 2. 请求
 		this.fetchTopMv()
+	},
+	// 上拉刷新
+	async onPullDownRefresh(){
+		// 1. 数据置空
+		this.setData({ videoList: []})
+		this.data.offset = 0
+		this.data.hasMore = true
+
+		// 2. 重新请求新的数据
+		await this.fetchTopMv()
+		// 3. 停止下拉刷新
+		wx.stopPullDownRefresh()
 	}
 })
